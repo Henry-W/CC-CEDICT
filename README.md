c8faf6ca4d1f978906bae070564f1234284f77d1
henrywhite <henry.white@capgemini.com>
2017-06-08 01:10:20 +0100

    Package and Main class created, readme updated.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # CC-CEDICT
+
+DSA Coursework Project Due 10th July 2017.
---
+++ src/aston/dsa/Main.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package aston.dsa;
+
+/**
+ * @author henrywhite
+ *
+ */
+public class Main {
+	
+	public Main() {
+		
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
